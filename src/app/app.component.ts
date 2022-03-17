2418d9b9d18d61c0575ed7318ea583765ed73cf3
AneiConstantin <anei.costantin@iisgalvanimi.edu.it>
2022-03-17 08:57:47 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+
+  list: string[] = []
+  testo1:string="button1";
+  testo2:string="button2";
+  testo3:string="button3";
+  testo4:string="button4";
+
+  onClick1(){
+    this.list.push(this.testo1)
+  }
+  onClick2(){
+    this.list.push(this.testo2)
+  }
+
+  onClick3(){
+    this.list.push(this.testo3)
+  }
+
+  onClick4(){
+    this.list.push(this.testo4)
+  }
 }
